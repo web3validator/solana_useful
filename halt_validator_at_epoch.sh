b91f308151a8256e22053409e0986206581c4c55
Nikita <nikitlysenko@gmail.com>
2024-01-01 16:03:44 -0700

    Update halt_validator_at_epoch.sh

--- halt_validator_at_epoch.sh
+++ halt_validator_at_epoch.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
+# Instructions for using this script:
+# 1) Download the script:
+# $ curl https://gist.githubusercontent.com/willhickey/0c90b7929550a08712cd9adeb8b693c1/raw/9daa76365a6522c82541e22ddd1fcb7bf396e587/halt_validator_at_epoch.sh > halt_validator_at_epoch.sh
+
+# 2) Edit the final line of this script: it should be a command that will halt your validator
+
+# 3) Make the script executable: 
+# $ chmod 700 halt_validator_at_epoch.sh
+
+# 4) Run this script in a screen session so it keeps running even if your terminal disconnects. 
+# Here's an example using the name "halt_579" for the scren session:
+# $ screen -S halt_579
+# You're now inside the new screen session.
+
+# 5)  Inside the screen session run the script:
+# $ ./halt_validator_at_epoch.sh 579
+
+# 6) Disconnect from the screen with Ctrl+a, Ctrl+d. The session is still running in the background. 
+
+# You can list your screen sessions with:
+# $ screen -ls
+# You can reconnect to the screen session to make sure the script is still running:
+# $ screen -r halt_579
+
 if [[ -z $1 ]]; then
   echo "Usage: $0 <epoch>"
   echo "Waits until the specified epoch, then waits for a new snapshot and halts the validator."
@@ -15,7 +39,7 @@ do
   if [[ $current_epoch = $epoch_to_halt ]]; then
     echo "$current_epoch is same as $epoch_to_halt. Initiating shutdown procedure."
     break
-  elif (( $current_epoch > $epoch_to_halt )); then
+  elif [[ $current_epoch > $epoch_to_halt ]]; then
     echo "Current epoch $current_epoch is greater than epoch to halt $epoch_to_halt. Bailing out."
     exit 1
   fi
